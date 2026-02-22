b14cd9ad77d126525cf0d3bf9dca3bcee45e635a
Jo Yie <joyieleong@gmail.com>
2026-02-23 02:30:16 +0800

    update kinesis project

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -11,13 +11,47 @@ export const projects = [
     content: [
       { type: "text", value: "A reimagination of the donation experience inspired by the magic of telekinesis." },
       { type: "image", src: "/media/projects/kinesis/kinesis_video_1.gif" },
-      { type: "title", value: "I. The Problem" },
-      { type: "text", value: "In an increasingly cashless world, donation boxes are overlooked and charities miss out on vital contributions. Moreover, digital transactions disappears without feedback, visibility or emotional resonance. How might we tangibilise the seperation between donor and beneficiary?" },
+      { 
+        type: "text", 
+        value: "In an increasingly cashless world, donation boxes are a forgotten relic of the past. Digital donations to charity disappear without feedback, visibility or emotional resonance. " 
+      },
       { type: "image", src: "/media/projects/kinesis/kinesis_5.png" },
-      { type: "image", src: "/media/projects/kinesis/kinesis_1.png" },
-      { type: "image", src: "/media/projects/kinesis/kinesis_2.png" },
-      { type: "image", src: "/media/projects/kinesis/kinesis_3.png" },
-      { type: "image", src: "/media/projects/kinesis/kinesis_4.png" },
+      {
+        type: "text",
+        value: "We identified a shared gap across both physical and online donation methods: a lack of meaningful feedback for donors. Whether it was the clink of a coin hitting the bottom of a donation box or a push notification from a bank, the lack of acknowledgement is disconnected from the act of generosity."
+      },
+      { type: "title", value: "How might we tangibilise the seperation between donor and beneficiary?"},
+      { type: "image", src: "/media/projects/kinesis/kinesis_video_2.gif" },
+      { 
+        type: "text",
+        value: "We designed a physical installation that lights up as pedestrians approach, inviting curiosity and engagement in a public space."
+      },
+      { type: "image", src: "/media/projects/kinesis/kinesis_video_3.gif" },
+      {
+        type: "text",
+        value: "When a phone is intentionally placed near the device, an NFC tag prompts the user to make a donation."
+      },
+      { type: "image", src: "/media/projects/kinesis/kinesis_video_4.gif" },
+      {
+        type: "text",
+        value: "Completing a donation triggers a seemingly magical visual transfer of light from the phone screen to the installation, mirroring the flow of generosity from benefactor to beneficiary."
+      },
+      { type: "image", src: "/media/projects/kinesis/kinesis_video_5.gif" },
+      {
+        type: "text",
+        value: "The installation responds by changing the colour, marking a positive change made by the donor, and sends the warm glow back to the user."
+      },
+      { type: "image", src: "/media/projects/kinesis/kinesis_video_6.gif" },
+      { type: "image", src: "/media/projects/kinesis/kinesis_video_8.gif" },
+      { type: "image", src: "/media/projects/kinesis/kinesis_video_7.gif" },
+      {
+        type: "text",
+        value: "As the light reenters the user’s phone as a symbol of gratitude, the user is able to learn more about the organisation and the impact of their contribution."
+      },
+      {
+        type: "title",
+        value: "This project explores and demonstrates how physical presence and digital transaction could seamlessly integrate to create a completely new donation experience that neither modality could achieve independently."
+      },
       { type: "video", src: "https://player.vimeo.com/video/643354890?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" },
     ]
   },
